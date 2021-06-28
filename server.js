5ca163d2d285bf58c2881a35a61c25d2be8426f0
Shane <shane.messmer1@gmail.com>
2021-06-28 16:24:51 -0400

    adds heroku

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const { animals } = require('./:data/animals'); 
 
+const PORT = process.env.PORT || 3001;
+
+
 const express = require('express');
 
 const app = express();
@@ -45,6 +48,6 @@ app.get('/api/animals', (req, res) => {
 })
 
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
 console.log('API server now on port 3001!');
 });
